7ec4f7eb5785fd8e4943764a00989ebd58862475
Jacob Fricke <jacob.fricke@sense-it.de>
2017-12-21 20:47:47 +0100

    add databeseManager property config, default: config param

--- src/databaseManager.js
+++ src/databaseManager.js
@@ -4,6 +4,12 @@
 			throw new Error("given parameter is not a valid JSON") ;
 		}
 
+		Object.defineProperty(databaseManager.prototype, "config", {
+			enumerable: true,
+			value: config,
+			writable: false,
+
+		}) ;
 	} ;
 
 	module.exports = databaseManager ;
