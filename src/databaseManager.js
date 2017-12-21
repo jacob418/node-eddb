20f5f12ed2afa5e579d4fc810e961319f4455161
Jacob Fricke <jacob.fricke@sense-it.de>
2017-12-21 20:46:23 +0100

    check if config param for databaseManager is valid JSON, throw error if not

--- src/databaseManager.js
+++ src/databaseManager.js
@@ -1,5 +1,8 @@
 (function(){
 	var databaseManager = function databaseManager(config){
+		if(config.constructor !== {}.constructor){
+			throw new Error("given parameter is not a valid JSON") ;
+		}
 
 	} ;
 
