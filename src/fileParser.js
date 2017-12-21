94a1c6a73cc21873d3fbade955f752593d0712db
Jacob Fricke <jacob.fricke@sense-it.de>
2017-12-21 10:42:28 +0100

    add function fileParser.prototype.readLine - read given file line by line and parse JSON on each

--- src/fileParser.js
+++ src/fileParser.js
@@ -6,5 +6,45 @@
 
 	} ;
 
+	fileParser.prototype.readFile = function readFile(fileName, cbLine, cbEnd){
+
+		fs.stat(fileName, function(err,fileStats){
+			if(err){
+				cbEnd(err)
+			}else {
+				if (fileStats.isFile()) {
+					var ifstream = fs.createReadStream(fileName) ;
+					var lines = 0 ;
+					var eof = false ;
+
+					const lineReader = readline.createInterface({
+						input: ifstream
+					});
+
+					ifstream.on('end', function(){
+						eof = true ;
+					}) ;
+
+					lineReader.on('line', function (lineText) {
+						if(!eof) {
+							lines++;
+							try {
+								var lineData = JSON.parse(lineText);
+								cbLine(null, lineData) ;
+							} catch (e) {
+								cbLine(e) ;
+							}
+						} else {
+							cbEnd(null,lines) ;
+						}
+					});
+				} else {
+					cbEnd(new Error("no file or file not found: '" + fileName + "'")) ;
+				}
+			}
+		}) ;
+
+	} ;
+
 	module.exports = fileParser ;
 })() ;
