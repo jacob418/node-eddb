d70c4f8c14f825a48a239c2b1b082bdf7f9c2d37
Jacob Fricke <jacob.fricke@sense-it.de>
2017-12-21 21:40:51 +0100

    connect to database in initDatabase

--- src/databaseManager.js
+++ src/databaseManager.js
@@ -16,7 +16,36 @@
 	} ;
 
 	databaseManager.prototype.initDatabase = function initDatabase(cb){
+		fs.readFile('./sql/createTable.sql', function(err, query) {
+			if(err){
+				cb(err) ;
+			} else {
+				var error = null ;
 
+				var connection = mysql.createConnection({
+					host: this.config.host,
+					user: this.config.user,
+					password: this.config.pass,
+					database: this.config.db
+				});
+
+				connection.on('error', function(err){
+					error = err ;
+				}) ;
+
+				connection.connect(function(err){
+					if(err || error !== null){
+						if(err === null){
+							err = error ;
+						}
+
+						cb(err) ;
+					} else {
+						// ....
+					}
+				});
+			}
+		});
 	} ;
 
 	module.exports = databaseManager ;
