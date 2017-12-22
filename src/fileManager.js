b2103537c812164d0e2f9956edb190ecf7a05804
Jacob Fricke <jacob.fricke@sense-it.de>
2017-12-22 13:38:54 +0100

    add function to compare two files and replace the first with the second if there is a difference

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,17 +1,46 @@
 (function() {
 	const fs = require('fs') ;
 	const readline = require('readline');
+	const async = require('async') ;
+	const hashFiles = require('hash-files');
 
 	var fileManager = function fileManager(eddbAPI) {
-		if(eddbAPI.constructor !== {}.constructor){
-			throw new Error("given parameter is not a valid JSON") ;
+		if (eddbAPI.constructor !== {}.constructor) {
+			throw new Error("given parameter is not a valid JSON");
 		}
 
 		Object.defineProperty(this, "eddbAPI", {
 			enumerable: true,
 			value: eddbAPI,
 			writable: false,
-		}) ;
+		});
+	} ;
+
+
+	// replace files[0] with files[1] if they are not equal
+	// cb => (err, replaced)
+	fileManager.prototype.replaceIfNotEqual = function replaceIfNotEqual(files, cb){
+		async.map(files, function(file, asyncCb){
+				hashFiles({files:[file]}, asyncCb) ;
+			},
+			function(err, results) {
+				if(err){
+					console.log(err) ;
+				} else {
+					if(results[0].indexOf(results[1]) === -1){
+						// delete orig file and rename temp file if create time differs
+						try {
+							fs.unlinkSync(files[0]);
+							fs.renameSync(files[1], files[0]);
+							cb(null, true) ;
+						} catch (e) {
+							cb(e) ;
+						}
+					} else {
+						cb(null,false) ;
+					}
+				}
+			});
 	} ;
 
 	fileManager.prototype.readFile = function readFile(fileName, cbLine, cbEnd){
