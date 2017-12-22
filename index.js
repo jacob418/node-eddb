3fe012c3e8cb55e9a026a60c4607154a3b3bc8f6
Jacob Fricke <jacob.fricke@sense-it.de>
2017-12-22 10:40:04 +0100

    add action 'init-db' calling databaseManager.initDatabase

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const databaseManager = require('./src/databaseManager')(config.mysql) ;
 	const action = (typeof process.argv[2] === "string" ? process.argv[2] : "") ;
 
 	switch(action) {
+		case "init-db":
+			databaseManager.initDatabase(function (err, result) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(result);
+				}
+			});
+			break ;
 		default:
 			throw new Error("no valid action specified") ;
 	}
