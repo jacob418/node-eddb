569d53838e127ac2a316b75cc761f8ee541a855a
Jacob Fricke <jacob.fricke@sense-it.de>
2017-12-22 10:39:29 +0100

    use first command line parameter as action to be performed

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const fileParser = require('./src/fileParser')() ;
 const databaseManager = require('./src/databaseManager')(config.mysql) ;
 
 (function(){
+	const action = (typeof process.argv[2] === "string" ? process.argv[2] : "") ;
 
+	switch(action) {
+		default:
+			throw new Error("no valid action specified") ;
+	}
 })() ;
