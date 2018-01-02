ad032cb57ea64f7d8ba88af75352ace1d7e2c05f
Jacob Fricke <jacob.fricke@sense-it.de>
2018-01-02 19:01:32 +0100

    convert json to internal format before emitting event

--- src/eddnListener.js
+++ src/eddnListener.js
@@ -37,7 +37,53 @@
 						if (schema === 'https://eddn.edcd.io/schemas/journal/1'
 							&& content.event === "FSDJump"
 							&& content.Population && content.Population > 0) {
-							fireEvt('starSystem', content) ;
+
+							var data = {} ;
+							data.name 				= content.StarSystem || "" ;
+							data.rulimgFraction 	= content.SystemFaction || null ;
+							data.updatedAt		 	= parseInt(Date.parse(content.timestamp)/1000) ;
+							data.security		 	= content.SystemSecurity || null ;
+							data.state			 	= content.PowerplayState || "None" ;
+							data.economy		 	= content.SystemEconomy  || null ;
+							data.x				 	= content.StarPos[0] || null ;
+							data.y				 	= content.StarPos[1] || null ;
+							data.z				 	= content.StarPos[2] || null ;
+							data.population		 	= content.Population || null ;
+							data.factions = [] ;
+							var factionData = {} ;
+							if(Array.isArray(content.Factions)) {
+								for (var i = 0; i < content.Factions.length; i++) {
+									factionData = {};
+									factionData.name				= content.Factions[i].Name;
+									factionData.allegiance			= content.Factions[i].Allegiance || null ;
+									factionData.goverment			= content.Factions[i].Government || null ;
+									factionData.influence			= content.Factions[i].Influence || null ;
+									factionData.state				= content.Factions[i].FactionState || null ;
+									factionData.recoveringStates	= [] ;
+									if (Array.isArray(content.Factions[i].RecoveringStates)) {
+										var recoveringState = {};
+										for (var j = 0; j < content.Factions[i].RecoveringStates.length; j++) {
+											recoveringState = {};
+											recoveringState.trend = content.Factions[i].RecoveringStates[j].Trend;
+											recoveringState.state = content.Factions[i].RecoveringStates[j].State;
+											factionData.recoveringStates.push(recoveringState);
+										}
+									}
+									factionData.pendingStates 		= [];
+									if (Array.isArray(content.Factions[i].PendingStates)) {
+										var pendingState = {};
+										for (var k = 0; k < content.Factions[i].PendingStates.length; k++) {
+											pendingState = {};
+											pendingState.trend = content.Factions[i].PendingStates[k].Trend;
+											pendingState.state = content.Factions[i].PendingStates[k].State;
+											factionData.pendingStates.push(pendingState);
+										}
+									}
+									data.factions.push(factionData);
+								}
+							}
+							
+							fireEvt('starSystem', data) ;
 						}
 					});
 				});
