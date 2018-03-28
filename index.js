f983348f3aad4604a4a0a0ba15b81878fe8103f1
Jacob Fricke <jacob.fricke@ymail.com>
2018-03-28 12:03:27 +0200

    Create logger for error and application

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const config = require('./config.json') ;
+const path = require('path') ;
 const fileManager = require('./src/fileManager')(config.eddb) ;
 const databaseManager = require('./src/databaseManager')(config.mysql) ;
 const eddnListener = require('./src/eddnListener') ;
 const queryScheduler = require('./src/queryScheduler') ;
+const logrotate = require('file-stream-rotator') ;
+const logger = require('log') ;
 
 (function(){
 	const action = (typeof process.argv[2] === "string" ? process.argv[2] : "listen") ;
+
+	Date.prototype.toString = Date.prototype.toISOString ;
+
+	var errorLogCnf = JSON.parse(JSON.stringify(config.log)) ;
+	errorLogCnf.filename = path.join(config.log.logdir, "error-%DATE%.log") ;
+	var errorLog = new logger(config.log.level, logrotate.getStream(errorLogCnf)) ;
+
+	var appLogCnf = JSON.parse(JSON.stringify(config.log)) ;
+	appLogCnf.filename = path.join(config.log.logdir, "application-%DATE%.log") ;
+	var appLog = new logger(config.log.level, logrotate.getStream(appLogCnf)) ;
 
 	switch(action) {
 		case "init-db":
